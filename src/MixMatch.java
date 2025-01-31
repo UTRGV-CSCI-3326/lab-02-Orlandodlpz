f46697c15b2a89dc29d3a95e3cf1bcb083a9d55e
Orlandodlpz <143850773+Orlandodlpz@users.noreply.github.com>
2025-01-31 20:08:33 +0000

    Declared variables and initialized them

---
+++ src/MixMatch.java
@@ -0,0 +1,30 @@
+public class MixMatch {
+
+    public static void main (String [] alo) {
+        // Assignment due 
+        int students = 15;
+        String assignment = "variable printing assigment";
+        String dueDate = "Sunday, February 1st";
+        System.out.println(students + " students have not completed " + assignment);
+        System.out.println(assignment + " is due on " + dueDate);
+
+        System.out.println();
+
+        // Bike Ride Activity
+        int registered = 50;
+        String beginning = "Sugar Rd & Schunior Rd";
+        String end = "Sugar Rd & FM 409";
+        System.out.println(registered + " people have registered for the Bike Ride");
+        System.out.println("The event will begin on " + beginning + " to " + end);
+
+        System.out.println();
+
+        // Sum operation
+        float x = 20;
+        float y = 33;
+        float z = 0;
+        z = x + y;
+        System.out.println("The result of x = " + x + " + " + "y = " + y + " is: " + z);
+    }
+
+}
